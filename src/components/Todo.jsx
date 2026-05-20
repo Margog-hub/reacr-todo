dfe2226b9e1155b8653e3818b974d1b3d8fbeaec
Marina Kilbas <margog1982@gmail.com>
2026-05-20 22:06:17 +0300

    додані події

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,6 +3,7 @@ import AddTaskForm from './AddTaskForm'
 import SearchTaskForm from './SearchTaskForm'
 import TodoInfo from './TodoInfo'
 import TodoList from './TodoList'
+import query from './../../node_modules/esquery/dist/esquery.esm';
 
 const Todo = () => {
   const tasks = [
@@ -10,15 +11,39 @@ const Todo = () => {
     { id: 'task-2', title: "погладити котика ", isDone: true },
   ]
 
+  const deleteAllTasks = () => {
+    console.log("All")
+  }
+  const deleteTask = (taskId) => {
+    console.log(taskId)
+    // tasks.filter((task => (task.id !== taskId)))
+  }
+
+  const toggleTaskComplete = (taskId, isDone) => {
+    console.log(` Задача ${taskId} ${isDone ? 'виконана' : 'не виконана'}`)
+  }
+
+  const filterTask = (query) => {
+    console.log(`Поиск: ${query}`)
+  }
+
+  const addTask = () => {
+    console.log(`Задача добавлена! `)
+  }
   return (
     <div className="todo">
       <h1 className="todo__title">To Do List</h1>
-      < AddTaskForm />
-      <SearchTaskForm />
+      < AddTaskForm addTask={addTask} />
+      <SearchTaskForm onSearchInput={filterTask} />
       <TodoInfo
         total={tasks.length}
-        done={tasks.filter(({ isDone }) => isDone).length} />
-      <TodoList tasks={tasks} />
+        done={tasks.filter(({ isDone }) => isDone).length}
+        onDeleteAllButtonClick={deleteAllTasks}
+      />
+      <TodoList
+        tasks={tasks}
+        onDeleteTaskButtonClick={deleteTask}
+        onTaskCompleteChange={toggleTaskComplete} />
     </div>
   )
 }
