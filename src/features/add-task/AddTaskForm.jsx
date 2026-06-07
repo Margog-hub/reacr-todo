bd14f75b7ac6fbb8dd58e69b25aa9454fa258db3
Marina Kilbas <margog1982@gmail.com>
2026-06-07 16:33:57 +0300

    доопрацювання

--- src/features/add-task/AddTaskForm.jsx
+++ src/features/add-task/AddTaskForm.jsx
@@ -6,17 +6,17 @@ import { TasksContext } from "@/entities/todo"
 
 const AddTaskForm = (props) => {
   const { styles } = props
-  const { addTask, newTasksTitle, setNewTasksTitle, newTaskInputRef } = useContext(TasksContext)
+  const { addTask, newTaskInputRef } = useContext(TasksContext)
   const [error, setError] = useState('')
-
+  const [newTasksTitle, setNewTasksTitle] = useState("")
   const clearNewTaskTitle = newTasksTitle.trim()
   const isNewTaskTitleEmply = clearNewTaskTitle.length === 0
 
   const onSubmit = (e) => {
     e.preventDefault()
     if (!isNewTaskTitleEmply) {
-      addTask(clearNewTaskTitle)
-      setNewTasksTitle('')
+      addTask(clearNewTaskTitle),
+     () => setNewTasksTitle('')
     }
   }
 
